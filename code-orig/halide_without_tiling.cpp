3212b929007344d4d1643b7f1988a6ae8327c442
UbaidDalvi <101918419+UbaidDalvi@users.noreply.github.com>
2023-11-23 13:46:47 +0530

    Create halide_without_tiling.cpp

---
+++ code-orig/halide_without_tiling.cpp
@@ -0,0 +1,62 @@
+/*
+g++ combine*.cpp -g -I /home/dalviubaid02/Downloads/halide_build/include/ -I /home/dalviubaid02/Downloads/halide_build/distrib/tools/ -L /home/dalviubaid02/Downloads/halide_build/distrib/lib/ -lHalide `libpng-config --cflags --ldflags` -ljpeg -lpthread -ldl -o halide -std=c++17 
+LD_LIBRARY_PATH=/home/dalviubaid02/Downloads/halide_build/distrib/lib/ ./halide
+*/
+
+
+#include<Halide.h>
+#include <iostream>
+#include<halide_image_io.h>
+using namespace Halide::Tools;
+
+using namespace Halide;
+using namespace std;
+
+int main() {
+     Buffer<uint8_t> input =load_image("./Mona_Lisa.jpg");
+    Var x, y, c;
+
+    string user_input;
+    cout << "Enter a processing choice ('bright', 'gray', 'blur'): ";
+    cin >> user_input;
+    Buffer<uint8_t> output(input.width(), input.height(), input.channels());
+
+
+    if (user_input == "bright") {
+        Func brighter;
+        brighter(x, y, c) = cast<uint8_t>(min(input(x, y, c) * 2.0f, 255.0f));
+        output = brighter.realize({input.width(), input.height(), input.channels()});
+        save_image(output, "Mona_Lisa_bright_Halide.png");
+    }
+    else if (user_input == "gray") {
+        Func grayscale;
+        grayscale(x, y) = cast<uint8_t>(
+            0.299f * input(x, y, 0) +
+            0.587f * input(x, y, 1) +
+            0.114f * input(x, y, 2)
+        );
+        output = grayscale.realize({input.width(), input.height()});
+
+        save_image(output, "Mona_Lisa_gray_Halide.png");
+    }
+    else if (user_input == "blur") {
+        Func clamped;
+        clamped(x,y,c)=input(clamp(x,0,input.width()-1),clamp(y,0,input.height()-1),c);
+        Func input_16;
+        input_16(x,y,c)=cast<uint16_t>(clamped(x,y,c));
+        Func blurx;
+        blurx(x,y,c)=(input_16(x-1,y,c)+2*input_16(x,y,c)+input_16(x+1,y,c))/4;
+        Func blury;
+        blury(x,y,c)=(blurx(x,y-1,c)+2*blurx(x,y,c)+blurx(x,y+1,c))/4;
+        Func blur;
+        blur(x,y,c)=cast<uint8_t>(blury(x,y,c));
+        Buffer<uint8_t> output=blur.realize({input.width(),input.height(),3});
+        save_image(output,"Mona_Lisa_blur_Halide.jpg");
+    }
+    else {
+        cerr << "Invalid processing choice." << endl;
+        return 1;
+    }
+
+    return 0;
+}
